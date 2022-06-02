bb989dfe9f2155118f09049cca40cdf064f5b1aa
Prateek3032 <98746910+Prateek3032@users.noreply.github.com>
2022-06-02 11:15:28 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    boolean isWinner=false;
+    boolean isWinner=false;  // initiating with boolean value False
     int imageClicked=-1;
     int player = 1; //cross
     int [][]winningStates={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
